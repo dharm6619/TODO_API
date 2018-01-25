d144fbf5350a0eb6601e2daadec82e9655f47e91
Aswin Devarajan <6303170+codexdelta@users.noreply.github.com>
2018-01-25 06:10:21 +0530

    var User not assigned properly
    You have to assign the required module to a variable.

--- UserController.js
+++ UserController.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded ({extended : true }));
-var User require('./User');
+var User = require('./User');
 
 router.post('/', function (req, res) {
 
