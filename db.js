b4d2f123a1d7a3b9676cab43f827813386192af1
Aswin Devarajan <6303170+codexdelta@users.noreply.github.com>
2018-01-25 06:12:11 +0530

    unescaped character error
    Do you really need those greater then and less then sign. 
    Dont keep such sensitive data on github. 
    put it in .env file and make sure it is in .gitignore

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://<dharm6619>:<abhimanyu1@>@ds111258.mlab.com:11258/myapp_note');
+mongoose.connect('mongodb://dharm6619:abhimanyu1@ds111258.mlab.com:11258/myapp_note');
